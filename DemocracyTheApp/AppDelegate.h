26578be60a0995b92ef2aec1c8c446e8189d9a41
roseleaf <pteroline@gmail.com>
2012-08-17 16:24:02 -0700

    Initial Commit

---
+++ DemocracyTheApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemocracyTheApp
+//
+//  Created by Rose CW on 8/17/12.
+//  Copyright (c) 2012 Rose CW/JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemocracyTheApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemocracyTheApp
+//
+//  Created by Rose CW on 8/17/12.
+//  Copyright (c) 2012 Rose CW/JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
